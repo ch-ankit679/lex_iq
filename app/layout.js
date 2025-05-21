d4704fe8bfbe746f78875f92c80228d3a4a4df4c
ch-ankit679 <130274978+ch-ankit679@users.noreply.github.com>
2025-05-22 03:43:26 +0530

    project completed

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,16 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "./Prism.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { AppContextProvider } from "@/context/AppContext";
-
+import { Toaster } from "react-hot-toast";
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
 });
 
 export const metadata = {
-  title: "lexIQ",
+  title: "Sierra",
   description: "Full Stack Project",
 };
 
@@ -18,9 +19,13 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
       <AppContextProvider>
     <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
+      <body className={`${inter.className} antialiased`}>
+        <Toaster toastOptions={
+          {
+            success:{style:{background:"black", color:"white"}},
+            error:{style:{background:"black", color:"white"}}
+          }
+        }/>
         {children}
       </body>
     </html>
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -8,18 +8,18 @@ import ChatLabel from './ChatLabel'
 const Sidebar = ({expand,setExpand}) => {
   
   const {openSignIn} = useClerk()
-  const {user} = useAppContext()
+  const {user, chats, createNewChat} = useAppContext()
   const[openMenu,setOpenMenu] = useState({id:0,open:false})
 
   return (
     <div className={`${expand ? 'p-4 w-64' : 'md:w-20 w-0 max-md:overflow-hidden'} flex flex-col justify-between bg-[#212327] pt-7 transition-all z-50 max-md:absolute max-md:h-screen`}>
       <div>
         <div className={`flex ${expand ? 'flex-row gap-10' : 'flex-col items-center gap-8'}`}>
-          <Image className={expand ? 'w-36 h-5 mt-2 mr-10 ml-3' : 'w-10'} src={expand ? assets.logo_text : assets.logo_icon} alt=''/>
+          <Image className={expand ? 'w-45 h-12 ml-3' : 'w-10'} src={expand ? assets.logo_text : assets.logo_icon} alt=''/>
           <div onClick={() => expand ? setExpand(false) : setExpand(true)} 
-          className='group relative flex items-center justify-center hover:bg-gray-500/20 transition-all duration-300 h-9 w-9 aspect-square-rounded-lg cursor-pointer'>
+          className='group relative flex items-center justify-center h-9 w-9 aspect-square-rounded-lg cursor-pointer'>
             <Image src={assets.menu_icon} alt='' className='md:hidden'/>
-            <Image src={expand ? assets.sidebar_close_icon : assets.sidebar_icon} alt='' className='hidden md:block w-7'/>
+            <Image src={expand ? assets.sidebar_close_icon : assets.sidebar_icon} alt='' className='hidden md:block w-7 mt-8 hover:bg-gray-500/20 transition-all duration-300'/>
             <div className={`absolute w-max ${expand ? 'left-1/2 -translate-x-1/2 top-12': '-top-12 left-0'}
               opacity-0 group-hover:opacity-100 transition bg-black text-white text-sm px-3 py-2 rounded-lg
               shadow-lg pointer-events-none`}>
@@ -28,7 +28,7 @@ const Sidebar = ({expand,setExpand}) => {
             </div>
           </div>
         </div>
-          <button className={`${expand ? 'bg-primary hover:opacity-90 rounded-2xl gap-2 p-2.5 w-max':
+          <button onClick={createNewChat} className={`${expand ? 'bg-primary hover:opacity-90 rounded-2xl gap-2 p-2.5 w-max':
             'group relative h-9 w-9 mx-auto hover:bg-gray-500/30 rounded-lg'} mt-8 flex items-center justify-center cursor-pointer`}>
               <Image className={expand ? 'w-6' : 'w-7'} src={expand ? assets.chat_icon : assets.chat_icon_dull} alt=''/>
               <div className='absolute w-max -top-12 -right-12 opacity-0 group-hover:opacity-100
@@ -40,7 +40,10 @@ const Sidebar = ({expand,setExpand}) => {
           </button>      
           <div className={`mt-8 text-white/25 text-sm ${expand ? 'block' : 'hidden'}`}>
             <p className='my-1'>Recents</p>
-            <ChatLabel openMenu={openMenu} setOpenMenu={setOpenMenu}/>
+            {
+              chats.map((chat,index)=><ChatLabel key={index} name={chat.name} id={chat._id} openMenu={openMenu} setOpenMenu={setOpenMenu}/>)
+            }
+            
           </div>  
       </div>
 
